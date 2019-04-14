37a976c8ab62cc5dca6ba570059afcb6b609fa19
piotrek <p.gajdzica@labmatic.com>
2019-04-14 14:54:51 +0200

    Updated coloring
    Now algae smell gives cell blue color element and foraminifera red - witch means that cell containing both smells is pink

--- smog/src/main/scala/pl/edu/agh/smog/SmogMain.scala
+++ smog/src/main/scala/pl/edu/agh/smog/SmogMain.scala
@@ -6,9 +6,12 @@ import com.typesafe.scalalogging.LazyLogging
 import pl.edu.agh.smog.algorithm.SmogMovesController
 import pl.edu.agh.smog.config.SmogConfig
 import pl.edu.agh.smog.model.parallel.SmogConflictResolver
-import pl.edu.agh.smog.model.{AlgaeCell, ForaminiferaCell}
+import pl.edu.agh.smog.model.{AlgaeCell, ForaminiferaCell, WindPropagation}
 import pl.edu.agh.xinuk.Simulation
 import pl.edu.agh.xinuk.model.{DefaultSmellPropagation, EmptyCell, Signal, SmellingCell}
+import scala.math.atan
+import scala.math.Pi
+import scala.math.log
 
 object SmogMain extends LazyLogging {
   private val configPrefix = "smog"
@@ -26,21 +29,29 @@ object SmogMain extends LazyLogging {
   var minSmellValue = 0.0
   var maxSmellValue = 0.0
 
+  private def min(a: Double, b: Double): Double ={
+    if (a < b) a else b
+  }
+  private def max(a: Double, b: Double): Double ={
+    if (a > b) a else b
+  }
+
   private def cellToColor(cell: SmellingCell): Color = {
-    val smellValue = cell.smell.foldLeft(0.0)(_ + _.foldLeft(0.0)(_ + _.value).toInt)
-    if(minSmellValue > smellValue) {
+    val smellAlgaeValue = cell.smell.foldLeft(0.0)(_ + _.foldLeft(0.0)((acc, next) => acc + max(next.value, 0.0)).toInt)
+    val smellForaminiferaValue = cell.smell.foldLeft(0.0)(_ + _.foldLeft(0.0)((acc, next) => acc + min(next.value, 0.0)).toInt)
+    if(minSmellValue > smellForaminiferaValue) {
       System.out.println(minSmellValue, maxSmellValue, cell.smell(0)(0))
-      minSmellValue = smellValue
+      minSmellValue = smellForaminiferaValue
     }
-    if(maxSmellValue < smellValue) {
+    if(maxSmellValue < smellAlgaeValue) {
       System.out.println(minSmellValue, maxSmellValue, cell.smell(0)(0))
-      maxSmellValue = smellValue
+      maxSmellValue = smellAlgaeValue
     }
     cell match {
       case AlgaeCell(_, _) => new Color(0, 128, 0)
       case ForaminiferaCell(_, _, _) => new Color(139, 69, 19)
 //      case EmptyCell(_) => new Color(0, 0, 255)
-      case EmptyCell(_) => new Color(if(smellValue < 0) 0 else (smellValue / maxSmellValue * 255.0).toInt, 0, if(smellValue > 0) 0 else (smellValue / minSmellValue * 255.0).toInt)
+      case EmptyCell(_) => new Color(((atan(log(-smellForaminiferaValue)) + Pi / 2) * 255.0 / Pi).toInt, 0, ((atan(log(smellAlgaeValue)) + Pi/ 2) * 255.0 / Pi ).toInt)
       case _ => Color.WHITE
     }
   }
@@ -48,7 +59,7 @@ object SmogMain extends LazyLogging {
   def main(args: Array[String]): Unit = {
     import pl.edu.agh.xinuk.config.ValueReaders._
     new Simulation[SmogConfig](configPrefix, metricHeaders, SmogConflictResolver,
-      DefaultSmellPropagation.calculateSmellAddendsStandard)(new SmogMovesController(_)(_),
+      WindPropagation.calculateSmellAddends)(new SmogMovesController(_)(_),
       { case cell: SmellingCell => cellToColor(cell) }
     ).start()
   }
